1a1dad0914c227360dd18d1ba27dad9ef8e36a4a
Peter Ndumia <ndegwandumia@gmail.com>
2022-07-02 13:31:39 +0300

    commit tree 92375448e2ea5701bc3a4906194bf8e1ffff4bc5

--- index.js
+++ index.js
@@ -1,36 +1,117 @@
-function myEach(){
-
-}
-function myFilter(){
-
-}
-function myFind(){
-
-}
-function myFirst(){
-
-}
-function myFlatten(){
-
-}
-function myKeys(){
-    
-}
-function myLast(){
-
-}
-function myMap(){
-
-}
-function myReduce(){
-
-}
-function mySize(){
-
-}
-function mySortBy(){
-
-}
-function myValues(){
-
-}
+function myEach (collection, foo) {
+    let newCollection = [...collection]
+    if (collection instanceof Array === false) {
+      newCollection = Object.values(collection)
+    }
+    for (let i = 0; i < newCollection.length; i++) {
+      foo(newCollection[i])
+    }
+    return collection
+  }
+  
+  function myMap(collection, foo) {
+    let newCollection = [...collection]
+    if ( collection instanceof Array === false) {
+      newCollection = Object.values(collection)
+    }
+    let otherCollection = []
+    for(let i = 0; i < newCollection.length; i++) {
+      let itemNew = foo(newCollection[i])
+      otherCollection.push(itemNew)
+    }
+    return otherCollection
+  }
+  
+  function myReduce(collection, foo, acc) {
+    let newCollection = [...collection]
+    if (collection instanceof Array === false) {
+      newCollection = Object.values(collection)
+    }
+    let current = acc
+    if(typeof acc !== "number"){
+      current = newCollection[0]
+      for(let i = 1; i < newCollection.length; i++) {
+        current = foo(current, newCollection[i], newCollection)
+      }
+    } else { 
+      for(let i = 0; i < newCollection.length; i++) {
+        current = foo(current, newCollection[i], newCollection)
+      }
+    }
+    return current
+  }
+  
+  function myFind(collection, predicate) {
+    let newCollection = [...collection]
+    if (collection instanceof Array === false) {
+      newCollection = Object.values(collection)
+    }
+    for (let i = 0; i < newCollection.length; i++) {
+      if (predicate(newCollection[i])) {
+        return newCollection[i]
+      }
+    }
+    return undefined
+  }
+  
+  function myFilter(collection, predicate) {
+    let newCollection = [...collection]
+    if (collection instanceof Array === false) {
+      newCollection = Object.values(collection)
+    }
+    const otherCollection = []
+    for (let i = 0; i < newCollection.length; i++) {
+      if (predicate(newCollection[i])) {
+        otherCollection.push(newCollection[i])
+      }
+    }
+    return otherCollection
+  }
+  
+  function mySize(collection) {
+    let newCollection = [...collection]
+    if (collection instanceof Array === false) {
+      newCollection = Object.values(collection)
+    }
+    return newCollection.length
+  }
+  
+  function myFirst(array, n) {
+    if (typeof n !== "number") {
+      return array[0]
+    } else {
+      let newArray = []
+      for (let i = 0; i < n; i++){
+        newArray.push(array[i])
+      }
+      return newArray
+    }
+  }
+  
+  function myLast(array, n) {
+    if (typeof n !== "number") {
+      return array[array.length - 1]
+    } else {
+      let newArray = []
+      for (let i = 1; i <= n; i++) {
+        newArray.unshift(array[array.length-i])
+      }
+      return newArray
+    }
+  }
+  
+  function myKeys (object) {
+    let arrayOfKeys = []
+    for (let key in object) {
+      arrayOfKeys.push(key)
+    }
+    return arrayOfKeys
+  }
+  
+  function myValues (object) {
+    let arrayOfValues = []
+    for (let key in object) {
+      arrayOfValues.push(object[key])
+    }
+    return arrayOfValues
+  }
